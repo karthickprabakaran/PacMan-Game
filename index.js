98eaeb84eb8905c8dbd97f78e0f539e8afa6fc6b
karthickprabakaran <karthickprabakran06@gmail.com>
2025-08-14 22:07:56 +0530

    Added the Base Files and added the board

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+let board;
+
+
+const rowCount = 21;
+const columnCount = 19;
+const tileSize = 32;
+const boardWidth = columnCount * tileSize;
+const boardHeight = rowCount * tileSize;
+let context;
+
+window.onload = function() {
+  board = document.getElementById("board");
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext("2d");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
